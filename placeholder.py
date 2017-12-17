cd327d4722cf1a6fa201124bf8470d0144217ede
Anuradha Weeraman <aweermaan@gmail.com>
2017-12-17 14:44:53 +0530

    Placeholder

--- placeholder.py
+++ placeholder.py
@@ -4,7 +4,13 @@ a = tf.placeholder(tf.float32)
 b = tf.placeholder(tf.float32)
 c = tf.add(a, b)
 d = a + b # same as above
+e = d * 2
 
 sess = tf.Session()
-print(sess.run(c, {a: 3, b: 5}))
-print(sess.run(d, {a: [2, 4], b: [1, 3]}))
+f = sess.run(c, {a: 3, b: 5})
+g = sess.run(d, {a: [2, 4], b: [1, 3]})
+h = sess.run(e, {a: 3, b: 3})
+
+print(f)
+print(g)
+print(h)
