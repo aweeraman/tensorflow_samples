828aa2d027a848e4733afb7824fe0e6b5cff1f7f
Anuradha Weeraman <aweermaan@gmail.com>
2017-12-17 20:30:33 +0530

    Updated

--- mnist_single_layer.py
+++ mnist_single_layer.py
@@ -1,8 +1,15 @@
+import os
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 MNIST_DIR = "/home/anuradha/wintermute/datasets/MNIST/"
 LOG_DIR = "/tmp/summaries"
+
+epochs = 1000
+learning_rate = 0.5
+batch_size = 100
 
 mnist = input_data.read_data_sets(MNIST_DIR, one_hot=True)
 
@@ -18,7 +25,7 @@ w_h = tf.summary.histogram("weights", W)
 b_h = tf.summary.histogram("biases", b)
 l_h = tf.summary.scalar("loss", cross_entropy)
 
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
@@ -26,9 +33,9 @@ writer = tf.summary.FileWriter(LOG_DIR, sess.graph)
 summaries = tf.summary.merge_all()
 sess.run(init)
 
-for step in range(1000):
-	batch_xs, batch_yx = mnist.train.next_batch(100)
-	sess.run(train_step, feed_dict={x: batch_xs, y_: batch_yx})
+for step in range(epochs):
+	batch_xs, batch_yx = mnist.train.next_batch(batch_size)
+	loss = sess.run(train_step, feed_dict={x: batch_xs, y_: batch_yx})
 
 	summ = sess.run(summaries, feed_dict={x: batch_xs, y_: batch_yx})
 	writer.add_summary(summ, global_step=step)
