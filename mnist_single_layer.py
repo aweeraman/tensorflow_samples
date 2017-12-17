18e0329ad2f3f9b6487da96946a5aea5db560868
Anuradha Weeraman <aweermaan@gmail.com>
2017-12-17 21:25:28 +0530

    Added scope names

--- mnist_single_layer.py
+++ mnist_single_layer.py
@@ -7,7 +7,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 MNIST_DIR = "/home/anuradha/wintermute/datasets/MNIST/"
 LOG_DIR = "/tmp/summaries"
 
-epochs = 1000
+epochs = 10000
 learning_rate = 0.5
 batch_size = 100
 
@@ -16,16 +16,20 @@ mnist = input_data.read_data_sets(MNIST_DIR, one_hot=True)
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
-y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder(tf.float32, [None, 10])
-
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
 w_h = tf.summary.histogram("weights", W)
 b_h = tf.summary.histogram("biases", b)
-l_h = tf.summary.scalar("loss", cross_entropy)
 
-train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
+with tf.name_scope("linear_model") as scope:
+	y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+with tf.name_scope("cost_function") as scope:
+	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+	l_h = tf.summary.scalar("loss", cross_entropy)
+
+with tf.name_scope("train") as scope:
+	train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
